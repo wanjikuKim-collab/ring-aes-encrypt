0902bfa312c207abbc44b69e3f77c50eac5e6317
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-03-12 10:04:31 +0300

    Implement error handling using match and custom error handling using the error enum

--- src/mp4_reader.rs
+++ src/mp4_reader.rs
@@ -1,30 +1,41 @@
 use std::fs::File;
-use std::io::{self,BufReader, Read};
+use std::io::{BufReader, Read, ErrorKind};
 
 pub struct MP4Reader {
     path: String,
     data: Vec<u8>,
 }
 
+#[derive(Debug)]
+pub enum MP4ReaderError {
+    FileNotFound(String),
+    PermissionDenied(String),
+    IoError(std::io::Error),
+}
+
 impl MP4Reader {
-    pub fn read_file(path: &str) -> Result<Self, io::Error> {
-        let mut mp4_file = File::open(path).expect("Failed to open file");
-
-        // reading file into a BufReader
-        let mut reader = BufReader::new(&mut mp4_file);
-        
-        //code to read MP4 file in a vector
-        let mut buffer = Vec::new();
-        reader.read_to_end(&mut buffer).expect("Failed to read file");
-
-        let mp4_reader = MP4Reader{
-            path: path,
-            data: buffer,
+    pub fn read_file(path: &str) -> Result<Self, MP4ReaderError> {
+        let mut file = match File::open(path) {
+            Ok(file) => file,
+            Err(err) => match err.kind() {
+                ErrorKind::NotFound => return Err(MP4ReaderError::FileNotFound(path.to_owned())),
+                ErrorKind::PermissionDenied => return Err(MP4ReaderError::PermissionDenied(path.to_owned())),
+                _ => return Err(MP4ReaderError::IoError(err)),
+            },
         };
 
-        Ok(Self{
-            path: path.to_owned(),
-            data: buffer
-        })    
+         // reading file into a BufReader
+        let mut reader = BufReader::new(&mut file);
+
+        let mut data = Vec::new();
+        match reader.read_to_end(&mut data) {
+            Ok(_) => Ok(Self { 
+                path: path.to_owned(), 
+                data: data 
+            }),
+            Err(err) => Err(MP4ReaderError::IoError(err)),
+        }
     }
 }
+
+
