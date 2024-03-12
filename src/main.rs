5dc928c7dfbd77e48aee8b6222e9c5bcb480e450
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-03-12 10:49:08 +0300

    Test mp4 file reading with println macro and pattern matching for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 mod mp4_reader;
+
 fn main() {
-    let _reader = mp4_reader::MP4Reader::read_file("How to use FFMPEG.mp4").expect("File not found");
+    let reader_a = mp4_reader::MP4Reader::read_file("/home/wanjiku/Development/code/rust/ring-aes-encrypt/src/How to use FFMPEG.mp4");
+
+    match reader_a {
+        Ok(reader) => {
+            // Access and print some bytes from the data
+            let data_slice = &reader.data[..10];
+            println!("Read some bytes from file: {:?}", data_slice);
+        },
+        Err(err) => {
+            println!("Error reading file: {:?}", err);
+            // Handle the error appropriately (e.g., exit program, retry)
+        },
+    }
 }
